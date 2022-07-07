d55ca2b08cd0df14fedce43d290d60913d377ac6
jokoul <74833985+jokoul@users.noreply.github.com>
2022-07-07 16:21:21 +0200

    end of products list

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: "Wax Slim Shirt",
+      slug: "wax-slim-shirt",
+      category: "Shirts",
+      image: "/images/p1.png", // 679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: "Wax",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Afriks Fit Shirt",
+      slug: "afriks-fit-shirt",
+      category: "Shirts",
+      image: "/images/p2.png",
+      price: 250,
+      countInStock: 20,
+      brand: "Afriks",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Wax Slim Pant",
+      slug: "wax-slim-pant",
+      category: "Pants",
+      image: "/images/p3.png",
+      price: 25,
+      countInStock: 15,
+      brand: "Wax",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality shirt",
+    },
+    {
+      name: "Afriks Fit Pant",
+      slug: "afriks-fit-pant",
+      category: "Pants",
+      image: "/images/p4.png",
+      price: 65,
+      countInStock: 5,
+      brand: "Afriks",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+  ],
+};
+
+export default data;
