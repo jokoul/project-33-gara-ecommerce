ae9d0744471dc0754c14c27ddd598a8835255a43
jokoul <74833985+jokoul@users.noreply.github.com>
2022-07-07 14:42:35 +0200

    set up git and folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">garatimbi</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
